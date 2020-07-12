3aa1884175d0d016d6beee116e1b3c3272ffb1de
Suriyon Pakkapol <32071383+60ss@users.noreply.github.com>
2020-07-12 20:26:05 +0700

    noscript

---
+++ js/editor.js
@@ -0,0 +1,7 @@
+"use strict";function FncBundle(){MostReq(),ScrollPos()}doc.addEventListener("click",function(){ResetThePage()}),doc.addEventListener("DOMContentLoaded",function(n){FncBundle()}),win.addEventListener("scroll",function(){FncBundle()}),win.addEventListener("resize",function(){FncBundle()}),window.onload=LoadTime;le() {
+	//MainList();
+	//BalanceMainList();
+	MostReq();
+	ScrollPos();
+}
+window.onload = LoadTime;
